3321ea3c74bbc9f62a8f7cc71a4e8a9c3c50dd8d
Jeremy Fox <foxjerem@gmail.com>
2014-08-18 21:32:14 +0100

    Added temperature floor/ceiling

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -12,6 +12,18 @@ describe('Thermostat:', function() {
       expect(thermostat.temperature).toEqual(20);
     });
 
+    it('should have a minimum temperature of 10', function() {
+      expect(thermostat.minimumTemperature()).toEqual(10);
+    });
+
+    it('should have a maximum temperature of 25', function() {
+      expect(thermostat.maximumTemperature()).toEqual(25);
+    });
+
+    it('should have power saving mode enabled', function() {
+      expect(thermostat.powerSaving).toBe(true);
+    });
+
   });
 
   describe('changing temperature', function() {
@@ -26,48 +38,32 @@ describe('Thermostat:', function() {
       expect(thermostat.temperature).toEqual(19);
     });
 
+    it('should not be able to increase temperature above the maximum', function() {
+      thermostat.temperature = thermostat.maximumTemperature();
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(25);
+    });
+
+    it('should not be able to decrease the temperature below the minimum', function () {
+      thermostat.temperature = thermostat.minimumTemperature();
+      thermostat.decreaseTemperature();
+      expect(thermostat.temperature).toEqual(10);
+    });
+
   });
 
-  
+  describe('power saving mode', function() {
 
-  // describe("when song has been paused", function() {
-  //   beforeEach(function() {
-  //     player.play(song);
-  //     player.pause();
-  //   });
+    it('should have a maximum temperature of 25 with power saving on', function() {
+      thermostat.powerSaving = true;
+      expect(thermostat.maximumTemperature()).toEqual(25);
+    });
 
-  //   it("should indicate that the song is currently paused", function() {
-  //     expect(player.isPlaying).toBeFalsy();
+    it('should have a maximum temperature of 32 with power saving off', function() {
+      thermostat.powerSaving = false;
+      expect(thermostat.maximumTemperature()).toEqual(32);
+    });
 
-  //     // demonstrates use of 'not' with a custom matcher
-  //     expect(player).not.toBePlaying(song);
-  //   });
+  });
 
-  //   it("should be possible to resume", function() {
-  //     player.resume();
-  //     expect(player.isPlaying).toBeTruthy();
-  //     expect(player.currentlyPlayingSong).toEqual(song);
-  //   });
-  // });
-
-  // // demonstrates use of spies to intercept and test method calls
-  // it("tells the current song if the user has made it a favorite", function() {
-  //   spyOn(song, 'persistFavoriteStatus');
-
-  //   player.play(song);
-  //   player.makeFavorite();
-
-  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  // });
-
-  // //demonstrates use of expected exceptions
-  // describe("#resume", function() {
-  //   it("should throw an exception if song is already playing", function() {
-  //     player.play(song);
-
-  //     expect(function() {
-  //       player.resume();
-  //     }).toThrowError("song is already playing");
-  //   });
-  // });
-});
+ });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -1,13 +1,22 @@
 function Thermostat() {
 	this.temperature = 20;
+	this.powerSaving = true;
 };
 
 Thermostat.prototype.increaseTemperature = function() {
-	this.temperature += 1;
+	this.temperature = Math.min(this.maximumTemperature(), this.temperature + 1);
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
-	this.temperature -= 1;
+	this.temperature = Math.max(this.minimumTemperature(), this.temperature - 1);
+};
+
+Thermostat.prototype.maximumTemperature = function() {
+	return (this.powerSaving ? 25 : 32);
+};
+
+Thermostat.prototype.minimumTemperature = function() {
+	return 10;
 };
 // Player.prototype.play = function(song) {
 //   this.currentlyPlayingSong = song;
